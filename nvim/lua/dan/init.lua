8d575c1d00d06f7136ea7d7f0544cab89ed32ac9
Dan Lionis <26198474+danlionis@users.noreply.github.com>
2023-10-27 15:28:07 +0200

    move .config to own folder

---
+++ nvim/lua/dan/init.lua
@@ -0,0 +1,4 @@
+require("dan.options")
+require("dan.keymaps")
+require("dan.autocmds")
+require("dan.netrw")
---
+++ nvim/lua/plugins/mason.lua
@@ -0,0 +1,23 @@
+return {
+    {
+        'williamboman/mason.nvim',
+        cmd    = "Mason",
+        opts   = {
+            -- PATH = "append",
+        },
+        lazy   = true,
+        config = true
+    },
+    {
+        "jay-babu/mason-null-ls.nvim",
+        event = { "BufReadPre", "BufNewFile" },
+        dependencies = {
+            "williamboman/mason.nvim",
+            "jose-elias-alvarez/null-ls.nvim",
+        },
+        opts = {
+            ensure_installed = nil,
+            automatic_installation = true
+        }
+    }
+}
