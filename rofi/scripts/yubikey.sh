2b6751a295c41b3602b37d8e988ac36bd131a392
Dan Lionis <26198474+danlionis@users.noreply.github.com>
2023-12-04 21:53:43 +0100

    hypr: replace rofi with tofi

--- rofi/scripts/yubikey.sh
+++ rofi/scripts/yubikey.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-selected=$(ykman oath accounts list | rofi -i -dmenu -p "2FA Codes")
+# cmd='rofi -i -dmenu -p "2FA Codes"'
+cmd='tofi --prompt-text="2FA Codes"'
+
+selected=$(ykman oath accounts list | eval $cmd)
 
 if [ -n "$selected" ]; then
     ykman oath accounts code --single $selected | wl-copy
