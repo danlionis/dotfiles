1c722e22e612a10d83df0aa1d99bd0ba316b0f8f
Dan Lionis <26198474+danlionis@users.noreply.github.com>
2024-01-13 16:23:35 +0100

    nvim: extract harpoon

---
+++ nvim/lua/plugins/harpoon.lua
@@ -0,0 +1,59 @@
+function harpoonSelectTable(keys)
+    for i = 1, 9 do
+        local entry = {
+            ("<C-" .. i .. ">"),
+            function() require("harpoon"):list():select(i) end,
+            desc = "Harpoon " .. i,
+            mode = "n"
+        }
+
+        table.insert(keys, entry)
+    end
+
+    return keys
+end
+
+local keys = {
+    {
+        "<leader>a",
+        function() require("harpoon"):list():append() end,
+        desc = "Harpoon Mark File",
+        mode = "n"
+    },
+    {
+        "<leader>ht",
+        function()
+            local harpoon = require("harpoon")
+            harpoon.ui:toggle_quick_menu(harpoon:list())
+        end,
+        desc = "[H]arpoon [T]oggle Menu",
+        mode = "n"
+    },
+    {
+        "<leader>hm",
+        ":Telescope harpoon marks<CR>",
+        desc = "[H]arpoon [M]arks",
+        mode = "n"
+    },
+};
+
+
+local allKeys = harpoonSelectTable(keys)
+
+return {
+    {
+        "ThePrimeagen/harpoon",
+        branch = "harpoon2",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            'nvim-telescope/telescope.nvim',
+        },
+        keys = allKeys,
+        config = function(_, opts)
+            local harpoon = require("harpoon")
+            harpoon:setup(opts)
+            require("telescope").load_extension("harpoon")
+        end
+    }
+
+}
