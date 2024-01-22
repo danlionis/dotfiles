4b10495b6a51d8ed832c86ce57f320858e5d9d9d
Dan Lionis <26198474+danlionis@users.noreply.github.com>
2024-01-22 23:36:43 +0100

    fish: remove fzf keybinds

--- fish/config.fish
+++ fish/config.fish
@@ -1,13 +1,5 @@
 set -gx EDITOR /usr/bin/env nvim
 
-fzf_key_bindings
-set -gx FZF_DEFAULT_OPTS "--height 40%"
-set -gx FZF_CTRL_T_OPTS "--border --preview 'bat --color=always {}'"
-set -gx FZF_CTRL_R_OPTS "--border"
-
-# set -gx FZF_DEFAULT_COMMAND 'fd --type f --hidden --follow --exclude .git'
-# set -gx FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
-
 source /home/dan/.config/fish/fish_greeting.fish
 
 if type -q starship
