ad56172b0d62dbd5a64d8b78316e4518820bbbee
Dan Lionis <26198474+danlionis@users.noreply.github.com>
2024-01-30 23:10:28 +0100

    nvim: colorscheme github

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,6 +1,7 @@
 return {
     {
         "catppuccin/nvim",
+        enabled = false,
         name = "catppuccin",
         opts = {
             -- transparent_background = true
@@ -9,6 +10,18 @@ return {
             require("catppuccin").setup(opts)
             vim.cmd.colorscheme("catppuccin")
         end
+    },
+    {
+        'projekt0n/github-nvim-theme',
+        lazy = false,    -- make sure we load this during startup if it is your main colorscheme
+        priority = 1000, -- make sure to load this before all the other start plugins
+        config = function()
+            require('github-theme').setup({
+                -- ...
+            })
+
+            vim.cmd('colorscheme github_dark_default')
+        end,
     }
     -- {
     --     "folke/tokyonight.nvim",
