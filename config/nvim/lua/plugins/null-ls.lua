65e93679bd25e2ad7800ca65ab6b3e29987a7264
Dan Lionis <26198474+danlionis@users.noreply.github.com>
2024-01-30 23:11:32 +0100

    nvim: add rustywind

--- config/nvim/lua/plugins/null-ls.lua
+++ config/nvim/lua/plugins/null-ls.lua
@@ -7,18 +7,15 @@ return {
 
             null_ls.setup({
                 sources = {
-                    null_ls.builtins.formatting.black,
-                    null_ls.builtins.formatting.beautysh,
-                    null_ls.builtins.formatting.prettier,
-                    null_ls.builtins.formatting.sql_formatter,
-                    -- null_ls.builtins.formatting.prettier.with({
-                    --     filetypes = { "html", "json", "yaml", "typescript", "javascript" },
-                    -- }),
-                    null_ls.builtins.formatting.markdownlint,
-                    null_ls.builtins.formatting.nixpkgs_fmt,
                     null_ls.builtins.diagnostics.mypy,
                     null_ls.builtins.diagnostics.ruff,
-                    -- null_ls.builtins.completion.spell,
+                    null_ls.builtins.formatting.beautysh,
+                    null_ls.builtins.formatting.black,
+                    null_ls.builtins.formatting.markdownlint,
+                    null_ls.builtins.formatting.nixpkgs_fmt,
+                    null_ls.builtins.formatting.prettier,
+                    null_ls.builtins.formatting.rustywind,
+                    null_ls.builtins.formatting.sql_formatter,
                     null_ls.builtins.hover.dictionary,
                 }
             })
