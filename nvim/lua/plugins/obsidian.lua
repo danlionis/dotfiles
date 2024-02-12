f5952dbff3c18b14dbcf1a6458decad4435441db
Dan Lionis <26198474+danlionis@users.noreply.github.com>
2024-02-12 23:09:59 +0100

    nvim: add obsidian

---
+++ nvim/lua/plugins/obsidian.lua
@@ -0,0 +1,59 @@
+return {
+    "epwalsh/obsidian.nvim",
+    -- tag = "v2.10.0", -- recommended, use latest release instead of latest commit
+    lazy = true,
+    ft = "markdown",
+    -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+    -- event = {
+    --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+    --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
+    --   "BufReadPre path/to/my-vault/**.md",
+    --   "BufNewFile path/to/my-vault/**.md",
+    -- },
+    dependencies = {
+        -- Required.
+        "nvim-lua/plenary.nvim",
+
+        -- see below for full list of optional dependencies 👇
+    },
+    opts = {
+        workspaces = {
+            {
+                name = "notes",
+                path = "~/Documents/obsidian-vault/",
+            },
+        },
+
+        notes_subdir = "zettelkasten",
+        new_notes_location = "notes_subdir",
+
+
+        -- Optional, customize how names/IDs for new notes are created.
+        note_id_func = function(title)
+            -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
+            local suffix = ""
+            if title ~= nil then
+                -- If title is given, transform it into valid file name.
+                suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+            else
+                -- If title is nil, just add 4 random uppercase letters to the suffix.
+                for _ = 1, 4 do
+                    suffix = suffix .. string.char(math.random(65, 90))
+                end
+            end
+            return tostring(os.time()) .. "-" .. suffix
+        end,
+
+
+        picker = {
+            name = "telescope.nvim",
+            mappings = {
+                -- Create a new note from your query.
+                new = "<C-x>",
+                -- Insert a link to the selected note.
+                insert_link = "<C-l>",
+            },
+        },
+
+    },
+}
