b2f82662078aadf301bea5e0538bfe9f1da9fd09
Dan Lionis <26198474+danlionis@users.noreply.github.com>
2024-02-12 23:11:15 +0100

    nvim: oil change keybindings

--- config/nvim/lua/plugins/oil.lua
+++ config/nvim/lua/plugins/oil.lua
@@ -1,11 +1,24 @@
 return {
     {
         'stevearc/oil.nvim',
-        opts = {},
+        opts = {
+            keymaps = {
+                ["<C-p>"] = false,
+            }
+        },
         lazy = false,
         keys = {
             { "-", "<CMD>Oil<CR>", { desc = "Open parent directory", mode = "n" } }
         },
+        config = function(_, opts)
+            require("oil").setup(opts)
+            vim.api.nvim_create_autocmd("FileType", {
+                pattern = "oil_preview",
+                callback = function(params)
+                    vim.keymap.set("n", "<CR>", "o", { buffer = params.buf, remap = true, nowait = true })
+                end,
+            })
+        end,
         -- Optional dependencies
         dependencies = { "nvim-tree/nvim-web-devicons" },
     }
