f830fe3f6e14d1d20b937620c2bc2c4b7c348074
Dan Lionis <26198474+danlionis@users.noreply.github.com>
2024-04-28 19:42:01 +0200

    nvim: current version

--- config/nvim/lua/plugins/ui.lua
+++ config/nvim/lua/plugins/ui.lua
@@ -30,24 +30,6 @@ return {
         end
     },
 
-    -- better vim.ui
-    {
-        "stevearc/dressing.nvim",
-        event = "VeryLazy",
-        init = function()
-            ---@diagnostic disable-next-line: duplicate-set-field
-            vim.ui.select = function(...)
-                require("lazy").load({ plugins = { "dressing.nvim" } })
-                return vim.ui.select(...)
-            end
-            ---@diagnostic disable-next-line: duplicate-set-field
-            vim.ui.input = function(...)
-                require("lazy").load({ plugins = { "dressing.nvim" } })
-                return vim.ui.input(...)
-            end
-        end,
-    },
-
     -- Add indentation guides even on blank lines
     {
         'lukas-reineke/indent-blankline.nvim',
