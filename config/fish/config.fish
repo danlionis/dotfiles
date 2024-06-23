e5f544cdea105ed8286ed1fde27110ab27f196bb
Dan Lionis <26198474+danlionis@users.noreply.github.com>
2024-06-23 15:29:15 +0200

    nix: add yazi

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -55,5 +55,16 @@ function lf
     end
 end
 
+if type -q yazi
+    function yy
+        set tmp (mktemp -t "yazi-cwd.XXXXXX")
+        yazi $argv --cwd-file="$tmp"
+        if set cwd (cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+            cd -- "$cwd"
+        end
+        rm -f -- "$tmp"
+    end
+end
+
 
 fish_vi_key_bindings
