ea26635d9cceacea54deec3d233ea38f2d67ceb7
Dan Lionis <26198474+danlionis@users.noreply.github.com>
2024-09-08 14:07:23 +0200

    nvim: use stylua formatter

--- config/nvim/lua/plugins/conform.lua
+++ config/nvim/lua/plugins/conform.lua
@@ -1,5 +1,5 @@
 return {
-    'stevearc/conform.nvim',
+    "stevearc/conform.nvim",
     opts = {
         formatters_by_ft = {
             python = { "ruff_organize_imports", "ruff_format" },
@@ -13,20 +13,22 @@ return {
             templ = { "templ", "rustywind", lsp_format = "fallback" },
 
             sql = { "sql_formatter" },
+
+            lua = { "stylua" },
         },
         formatters = {
             sql_formatter = {
                 prepend_args = {
-                    "-c", ".sql-formatter.json" -- default in "sql-formatter@15.4.0, but not yet in nixpkgs"
-                }
-            }
+                    "-c",
+                    ".sql-formatter.json", -- default in "sql-formatter@15.4.0, but not yet in nixpkgs"
+                },
+            },
         },
         format_on_save = {
             -- I recommend these options. See :help conform.format for details.
             lsp_format = "fallback",
             timeout_ms = 500,
         },
-
     },
     config = function(_, opts)
         require("conform").setup(opts)
@@ -37,5 +39,5 @@ return {
                 require("conform").format({ bufnr = args.buf })
             end,
         })
-    end
+    end,
 }
