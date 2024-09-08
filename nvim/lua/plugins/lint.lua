53998afa7d4cac99c19172647ecb6ce650ea859b
Dan Lionis <26198474+danlionis@users.noreply.github.com>
2024-09-08 12:39:32 +0200

    nvim: replace none-ls with conform & nvim-lint

---
+++ nvim/lua/plugins/lint.lua
@@ -0,0 +1,21 @@
+return {
+    {
+        "mfussenegger/nvim-lint",
+        config = function()
+            local lint = require('lint')
+
+            lint.linters_by_ft = {
+                python = { "ruff", }
+            }
+
+            vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+                callback = function()
+                    -- try_lint without arguments runs the linters defined in `linters_by_ft`
+                    -- for the current filetype
+                    require("lint").try_lint()
+                end,
+            })
+        end
+
+    }
+}
