fce7f1bf3dc70b3d7aa0784636ca581d82f9c89e
Dan Lionis <26198474+danlionis@users.noreply.github.com>
2024-09-14 17:22:14 +0200

    nvim: supermaven: add toggle keybind

--- nvim/lua/plugins/supermaven.lua
+++ nvim/lua/plugins/supermaven.lua
@@ -1,10 +1,13 @@
 return {
     "supermaven-inc/supermaven-nvim",
-    enabled = false,
     opts = {
         keymaps = {
             accept_suggestion = "<C-a>",
-        }
+        },
+    },
+    lazy = false,
+    keys = {
+        { "<leader>smt", "<cmd>SupermavenToggle<cr>", desc = "[S]uper[M]aven [T]oggle" },
     },
     config = function(_, opts)
         require("supermaven-nvim").setup(opts)
