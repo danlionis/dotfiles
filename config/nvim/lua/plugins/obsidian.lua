57d0dd96a4d66dbfbcebbbdd2c433a1befe1d8a3
Dan Lionis <26198474+danlionis@users.noreply.github.com>
2025-02-03 23:18:11 +0100

    nvim: setup for quartz

--- config/nvim/lua/plugins/obsidian.lua
+++ config/nvim/lua/plugins/obsidian.lua
@@ -22,7 +22,7 @@ return {
         workspaces = {
             {
                 name = "notes",
-                path = "~/Documents/notes/",
+                path = "~/Documents/notes/vault",
             },
         },
 
@@ -136,6 +136,25 @@ return {
         ---@param url string
         follow_url_func = function(url)
             vim.ui.open(url) -- need Neovim 0.10.0+
+        end,
+
+        note_frontmatter_func = function(note)
+            -- Add the title of the note as an alias.
+            if note.title then
+                note:add_alias(note.title)
+            end
+
+            local out = { id = note.id, aliases = note.aliases, tags = note.tags, title = note.title, publish = false }
+
+            -- `note.metadata` contains any manually added fields in the frontmatter.
+            -- So here we just make sure those fields are kept in the frontmatter.
+            if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
+                for k, v in pairs(note.metadata) do
+                    out[k] = v
+                end
+            end
+
+            return out
         end,
 
         picker = {
