84e8a9b310a52bba3f359a6e23f5118b69e49ecb
Dan Lionis <26198474+danlionis@users.noreply.github.com>
2025-08-19 21:01:30 +0200

    kmk: add speedtyping layer toggle

--- kmk/sweep/code.py
+++ kmk/sweep/code.py
@@ -4,8 +4,6 @@ import supervisor
 
 # import neopixel
 from kb import KMKKeyboard
-from storage import getmount
-
 from kmk.keys import KC
 from kmk.modules.capsword import CapsWord
 from kmk.modules.combos import Combos
@@ -15,6 +13,7 @@ from kmk.modules.mouse_keys import MouseKeys
 from kmk.modules.split import Split, SplitSide
 from kmk.modules.sticky_keys import StickyKeys
 from kmk.modules.tapdance import TapDance
+from storage import getmount
 
 supervisor.set_next_code_file(filename="code.py", reload_on_error=True)
 
@@ -180,7 +179,7 @@ keyboard.keymap = [
                                             _______,    _______,    _______,    _______,
     ],
     [  # FN (4)
-        KC.PSCR,    KC.F7,      KC.F8,      KC.F9,      KC.F10,     _______,    KC.TG(6),   KC.TG(5),   KC.TG(8),   KC.RESET,
+        KC.PSCR,    KC.F7,      KC.F8,      KC.F9,      KC.F10,     _______,    KC.TG(6),   KC.TG(5),   KC.TG(7),   KC.RESET,
         _______,    KC.F1,      KC.F2,      KC.F3,      KC.F11,     KC.MS_LT,   KC.MS_DN,   KC.MS_UP,   KC.MS_RT,   _______,
         _______,    KC.F4,      KC.F5,      KC.F6,      KC.F12,     _______,    KC.MB_LMB,  KC.MB_RMB,  _______,    _______,
                                             _______,    _______,    _______,    _______,
