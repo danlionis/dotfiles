89d968f5bf95677a198d6ad5009d9cb8ebb188a0
Dan Lionis <26198474+danlionis@users.noreply.github.com>
2025-08-19 20:57:29 +0200

    rofi: display 2fa code when copying

--- rofi/scripts/yubikey.sh
+++ rofi/scripts/yubikey.sh
@@ -3,9 +3,10 @@
 # cmd='rofi -i -dmenu -p "2FA Codes"'
 cmd='tofi --prompt-text="2FA Codes"'
 
-selected=$(ykman oath accounts list | eval $cmd)
+selected=$(ykman oath accounts list | eval "$cmd")
 
 if [ -n "$selected" ]; then
-    ykman oath accounts code --single $selected | wl-copy
-    notify-send "Code for $selected copied to clipboard"
+    code=$(ykman oath accounts code --single "$selected")
+    echo "$code" | wl-copy
+    notify-send "Code for $selected copied to clipboard: $code"
 fi
