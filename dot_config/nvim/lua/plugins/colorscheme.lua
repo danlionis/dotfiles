4d57786c48b9d65e7794f0ffab41a66e1a971c8b
Dan Lionis <26198474+danlionis@users.noreply.github.com>
2026-06-28 10:32:27 +0200

    nvim: oled background on work machine

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -16,8 +16,21 @@ return {
         lazy = false,    -- make sure we load this during startup if it is your main colorscheme
         priority = 1000, -- make sure to load this before all the other start plugins
         config = function()
+            local specs = {}
+            if vim.fn.hostname() == "work" then
+                specs = {
+                    all = {
+                        bg0 = "#000000", -- darkest background (status line, float windows, etc)
+                        bg1 = "#000000", -- main editor background
+                        bg2 = "#000000", -- cursor line / folds
+                        bg3 = "#000000", -- lighter shades
+                        bg4 = "#000000",
+                    }
+                }
+            end
+
             require('github-theme').setup({
-                -- ...
+                specs = specs,
             })
 
             vim.cmd('colorscheme github_dark_default')
